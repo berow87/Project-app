78a6c696a5bf4b793801218ee719b715e6ffc763
berow87 <berow_sport2010@hotmail.com>
2024-02-28 00:55:15 -0300

    login e cadastro

--- App.js
+++ App.js
@@ -1,20 +1,118 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default function App() {
+
+const Login = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  <View style={styles.container}>
+        
+        <Icon name="users" size={80} color="#900" style={styles.icone}/>
+        
+        
+        <Text style={styles.title}>Email</Text>
+        <TextInput
+        style={styles.input}
+        
+        />
+    
+        <Text style={styles.title}>Senha</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry
+        />
 
-const styles = StyleSheet.create({
+        <TouchableOpacity style={styles.loginButton}>
+          <Text style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.signupButton} >
+          <Text style={styles.buttonText}>CADASTRE-SE</Text>
+        </TouchableOpacity>
+
+  </View>
+  );
+  };
+
+  const Cadastro = () => {
+    return(
+      <View style={styles.container}>
+        
+        <Text style={styles.cabecalho}>Cadastro</Text>
+      
+        <Text style={styles.title}>Nome</Text>
+          <TextInput
+          style={styles.input}
+        
+          />
+
+        <Text style={styles.title}>Email</Text>
+          <TextInput
+          style={styles.input}
+        
+          />
+
+        <Text style={styles.title}>Senha</Text>
+          <TextInput
+          style={styles.input}
+          secureTextEntry
+          />
+
+      </View>
+    )
+  }
+  
+  const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
   },
-});
+  title: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 1,
+    right: 85,
+  },
+  cabecalho: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginBottom: 150,
+    
+  },
+  icone:{
+    marginBottom: 25,
+  },
+  input: {
+    width: '60%',
+    height: 40,
+    borderColor: 'gray',
+    color: 'gray',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 16,
+    paddingHorizontal: 10,
+  },
+  loginButton: {
+    backgroundColor: 'green',
+    paddingVertical: 12,
+    paddingHorizontal: 48,
+    borderRadius: 10,
+    marginBottom: 12,
+    marginTop: 15,
+  },
+  signupButton: {
+    backgroundColor: 'green',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  });
+  
+  export default Login;
